2b70e5823a02304543d0350284d7c17bdfd93462
alphaASC5 <barriea@bxscience.edu>
2018-07-12 11:44:27 -0300

    I made a bunch of functions.

---
+++ notSoRandom.js
@@ -0,0 +1,37 @@
+// 1. function randomNumber(num){
+//     const decimal = Math.random();
+//     const times = num * decimal
+//     const rounded = Math.floor(times);
+//     console.log(rounded);
+// }
+
+// randomNumber(30);
+
+function randomLetter(word){
+    character = word.charAt(1);
+    console.log(character);
+}
+
+randomLetter('jeff');
+
+// 3. function myGuy(phrase){
+//     console.log(phrase + ", my guy");
+// }
+
+// myGuy('Wassup');
+
+// 4. function string_N_times(word, num){
+//     for(i = 0; i < num; i++){
+//         console.log(word);
+//     }
+// }
+
+// string_N_times('yes', 17);
+
+// 5. function hypotenuse(a, b){
+//     c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//     console.log(c);
+// }
+
+// hypotenuse(3, 4);
+
